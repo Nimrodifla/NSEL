6c1d7a2f9cb99c9bb6717048a93918c97a153f5b
Nimrod Flaschner <nimrodifla@gmail.com>
2022-03-04 19:52:13 +0200

    v2

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-import { ServExNim } from './NSEL.js'; // important! to import my class in package.json add "type": "module"
+import * as NSEL from './NSEL.js'; // important! to import my class in package.json add "type": "module"
 
-let nim = new ServExNim();
-console.log(nim.db);
+// NSEL INIT
+const db = new NSEL.DB('host', 'user', 'password', 'database').db; // json data structure
+NSEL.listen(); // start listening (default: port 80 || process.env.PORT)
+const app = NSEL.app;
+
+// End Points
+app.get('/', (req, res)=>{
+    res.send('hello world');
+});
